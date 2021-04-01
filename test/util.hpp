f8f24583790c8d6850f87ab4019e3c628f9b9db4
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2021-04-01 16:40:15 -0400

    Rename folder.

---
+++ test/util.hpp
@@ -0,0 +1,23 @@
+#ifndef _UTIL_HPP_
+#define _UTIL_HPP_
+
+/********************* Header Files ***********************/
+#include <exception>
+#include <iostream>
+#include <set>
+#include <vector>
+#include <cmath>
+
+namespace game {
+
+/******************* Constants/Macros *********************/
+
+
+/******************* Type Definitions *********************/
+
+
+/************** Class & Func Declarations *****************/
+
+
+#endif /* _UTIL_HPP_ */
+
