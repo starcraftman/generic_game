2599988e0389829c0012c85ecbf581940f678713
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2021-04-02 21:09:23 -0400

    Install script for dependencies.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 This is just a place to experiment with design of a small C++ rpg dice type framework.
 I don't have a big plan at the moment but will see where it goes.
+
+# How To Build
+
+- Be on linux.
+- Run `install.sh`, should probably merge into cmake at some point.
+- Build like normal cmake project.
---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# Run this from the root of this project.
+URL_GTEST="https://github.com/google/googletest/archive/refs/tags/release-1.10.0.tar.gz"
+URL_BOOST="https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.tar.gz"
+TOP=$(pwd)
+DEPS_FOLDER="$(realpath ./deps)"
+
+build_google_test() {
+    local arc
+    local folder
+    arc="$(basename $URL_GTEST)"
+    folder="googletest-${arc%.tar.gz}"
+
+    wget $URL_GTEST
+    tar -xvf "$arc"
+    cd "$folder"
+
+    mkdir build
+    cd build
+    cmake ..
+    make
+
+    cd ..
+    cp ./build/lib/*.a "$DEPS_FOLDER/lib"
+    cp -r ./googletest/include/gtest/ ./googlemock/include/gmock/ "$DEPS_FOLDER/include"
+
+    cd $DEPS_FOLDER
+    command rm -rf "$arc" "$folder"
+}
+
+build_boost() {
+    local arc
+    local folder
+    arc="$(basename $URL_BOOST)"
+    folder="${arc%.tar.gz}"
+
+    wget $URL_BOOST
+    tar -xvf "$arc"
+    cd "$folder"
+
+    ./bootstrap.sh
+    ./b2 --prefix="$DEPS_FOLDER" -a install
+
+    cd $DEPS_FOLDER
+    command rm -rf "$arc" "$folder"
+}
+
+entry() {
+    mkdir -p "$DEPS_FOLDER/lib" "$DEPS_FOLDER/include"
+    cd "$DEPS_FOLDER"
+
+    build_google_test
+    build_boost
+
+    cd $TOP
+}
+entry
