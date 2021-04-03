cc380c1ec9e59c683e34dbef24b39acaf2a9a50e
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2021-04-02 22:26:10 -0400

    Just capture args.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,12 @@
 using std::cout;
 using std::endl;
 
-int main(){
+int main(int argc, char *argv[]){
+    cout << "Args being printed: " << endl;
+    for (int i = 0; i != argc; ++i) {
+        cout << "Argv[" << i << "] = " << argv[i] << endl;
+    }
+
     cout << "Generic game here." << endl;
     cout << "Rolled: " << util::d6() << endl;
 }
