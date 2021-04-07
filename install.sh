d69156996c1a95156afc0a22b008a38ccba8afa1
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2021-04-07 07:46:15 -0400

    Add SDL 1.2 to possible build in install.sh
    - Is EOL so required small patch.

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 # Run this from the root of this project.
 URL_GTEST="https://github.com/google/googletest/archive/refs/tags/release-1.10.0.tar.gz"
 URL_BOOST="https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.tar.gz"
+URL_SDL12="https://github.com/libsdl-org/SDL-1.2/archive/refs/tags/release-1.2.15.tar.gz"
 URL_SDL20="https://www.libsdl.org/release/SDL2-2.0.14.tar.gz"
 TOP=$(pwd)
 DEPS_FOLDER="$(realpath ./deps)"
@@ -46,6 +47,50 @@ build_boost() {
     command rm -rf "$arc" "$folder"
 }
 
+build_sdl12() {
+    local arc
+    local folder
+    arc="$(basename $URL_SDL12)"
+    folder="SDL-1.2-${arc%.tar.gz}"
+
+    wget $URL_SDL12
+    tar -xvf "$arc"
+    cd "$folder"
+
+    # patch needed
+    local patch_str
+    patch_str="
+diff -ur ../SDL-1.2-release-1.2.15/src/video/x11/SDL_x11sym.h ./src/video/x11/SDL_x11sym.h
+--- ../SDL-1.2-release-1.2.15/src/video/x11/SDL_x11sym.h	2012-01-20 19:49:48.000000000 -0500
++++ ./src/video/x11/SDL_x11sym.h	2021-04-07 07:22:57.195918036 -0400
+@@ -163,11 +163,11 @@
+  * Not required...these only exist in code in headers on some 64-bit platforms,
+  *  and are removed via macros elsewhere, so it's safe for them to be missing.
+  */
+-#ifdef LONG64
+-SDL_X11_MODULE(IO_32BIT)
+-SDL_X11_SYM(int,_XData32,(Display *dpy,register long *data,unsigned len),(dpy,data,len),return)
+-SDL_X11_SYM(void,_XRead32,(Display *dpy,register long *data,long len),(dpy,data,len),)
+-#endif
++// #ifdef LONG64
++// SDL_X11_MODULE(IO_32BIT)
++// SDL_X11_SYM(int,_XData32,(Display *dpy,register long *data,unsigned len),(dpy,data,len),return)
++// SDL_X11_SYM(void,_XRead32,(Display *dpy,register long *data,long len),(dpy,data,len),)
++// #endif
+
+ /*
+  * libX11 1.4.99.1 added _XGetRequest, and macros use it behind the scenes.
+"
+    echo "$patch_str" | patch -p0
+
+    ./autogen.sh
+    ./configure --prefix="$DEPS_FOLDER"
+    make install
+
+    cd $DEPS_FOLDER
+    command rm -rf "$arc" "$folder"
+}
+
 build_sdl20() {
     local arc
     local folder
@@ -69,6 +114,7 @@ entry() {
 
     build_google_test
     build_boost
+    # build_sdl12
     # build_sdl20
 
     cd $TOP
